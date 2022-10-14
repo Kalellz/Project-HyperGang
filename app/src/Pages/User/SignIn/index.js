06e2cd3e49c57e1f7f447bad48130ad0472c3f2b
kalel <ZoroPlay1220@gmail.com>
2022-10-14 02:02:59 -0300

    login 50%

--- app/src/Pages/User/SignIn/index.js
+++ app/src/Pages/User/SignIn/index.js
@@ -1,10 +1,31 @@
 import '../Log.scss';
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { Login } from "../../../api/userApi.js";
 function Home() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState()
+    const [senha, setSenha] = useState()
+    const [erro, setErro] = useState('')
+    async function entrarClick() {
+        try {
+            const r = await Login(email, senha);
+            if (r.status === 400) {
+                setErro(r.data.erro);
+            } else {
+                navigate('/')
+            }
+        } catch (err) {
+            if (err.response.status === 400)
+                setErro(err.response.data.erro)
+        }
+    }
+    
     return (
         <main className='Sign-Main'>
             <div className='Sign-ButtonTo-Signup'>
-                <button class="cta">
-                    <span class="hover-underline-animation"> Cadastre-Se </span>
+                <button className="cta" onClick={() => navigate('/cadastro')}>
+                    <span className="hover-underline-animation">Cadastre-Se</span>
                     <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
                         <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
                     </svg>
@@ -14,19 +35,22 @@ function Home() {
                 <h1 className='Sign-Title'>Seja bem vindo de volta!</h1>
                 <div className='Sign-Infos'>
                     <div className='Sign-Inputs'>
-                        <div class="form__group field">
-                            <input required="" placeholder="Name" class="form__field" type="email" />
-                            <label class="form__label" for="name">Email</label>
+                        <div className="form__group field">
+                            <input placeholder="Name" className="form__field" type="email" value={email} onChange={e => setEmail(e.target.value)} />
+                            <label className="form__label" for="name">Email</label>
                         </div>
                         <div class="form__group field">
-                            <input required="" placeholder="Name" class="form__field" type="input" />
-                            <label class="form__label" for="name">Senha</label>
+                            <input placeholder="Name" className="form__field" type="input" value={senha} onChange={e => setSenha(e.target.value)} />
+                            <label className="form__label" for="name">Senha</label>
                         </div>
                     </div>
                     <div className='Sign-Button'>
-                        <button>
+                        <button onClick={() => entrarClick()}>
                             <span>Estou Pronto!</span><i></i>
                         </button>
+                    </div>
+                    <div className="Sign-Erro">
+                        {erro}
                     </div>
                 </div>
             </section>
--- app/src/Pages/User/SignUp/index.js
+++ app/src/Pages/User/SignUp/index.js
@@ -1,10 +1,12 @@
 import '../Log.scss';
+import { useNavigate } from "react-router-dom";
 function Home() {
+    const navigate = useNavigate();
     return (
         <main className='Sign-Main'>
             <div className='Sign-ButtonTo-Signup'>
-                <button class="cta">
-                    <span class="hover-underline-animation"> Login </span>
+                <button class="cta" onClick={() => navigate('/login')}>
+                    <span class="hover-underline-animation">Login</span>
                     <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
                         <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
                     </svg>
@@ -27,7 +29,7 @@ function Home() {
                             <label class="form__label" for="name">Email</label>
                         </div>
                         <div class="form__group field">
-                            <input required="" placeholder="Name" class="form__field" type="input" />
+                            <input required="" placeholder="Name" class="form__field" type="password" />
                             <label class="form__label" for="name">Senha</label>
                         </div>
                     </div>
